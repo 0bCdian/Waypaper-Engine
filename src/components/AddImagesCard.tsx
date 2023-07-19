f9239b0cbd9944707906f73aa8d71930a44acf2c
0bCdian <diegomiguelp1996@gmail.com>
2023-07-19 12:08:26 +0200

    small tailwind fixes in ImageCard

--- src/components/AddImagesCard.tsx
+++ src/components/AddImagesCard.tsx
@@ -32,8 +32,8 @@ export const AddImagesCard: FC<AddImagesCardProps> = ({
       })
   }
   const styles = alone
-    ? 'relative rounded-lg max-w-fit mb-4 hover:bg-[#323232] active:scale-95 transition-all ease-in-out '
-    : 'relative rounded-lg bg-[#323232] hover:bg-[#424242] active:scale-95 transition-all max-w-fit mb-4'
+    ? 'cursor-pointer relative rounded-lg max-w-fit mb-4 hover:bg-[#323232] active:scale-95 transition-all ease-in-out '
+    : 'cursor-pointer relative rounded-lg bg-[#323232] hover:bg-[#424242] active:scale-95 transition-all max-w-fit mb-4'
   return (
     <div className={styles} onClick={isActive ? openFiles : undefined}>
       <div className=' flex justify-center  rounded-lg min-w-[300px] min-h-[200px]'>
--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,11 +1,11 @@
-import { useRef, type FC, useState, useEffect } from 'react'
+import { useRef, type FC, useId } from 'react'
 interface ImageCardProps {
   imageName: string
 }
 
 export const ImageCard: FC<ImageCardProps> = ({ imageName }) => {
-  const [isChecked, setChecked] = useState(false)
   const inputRef = useRef<HTMLInputElement>(null)
+  const id = useId()
   const imageNameFilePath =
     'atom://' +
     window.API_RENDERER.thumbnailDirectory +
@@ -14,35 +14,26 @@ export const ImageCard: FC<ImageCardProps> = ({ imageName }) => {
   const setImage = () => {
     window.API_RENDERER.setImage(imageName)
   }
-  useEffect(() => {
-    if (isChecked) {
-      if (inputRef.current !== null) {
-        inputRef.current.className = baseStyle + ' opacity-100'
-      }
-    } else {
-      if (inputRef.current !== null) {
-        inputRef.current.className = baseStyle + ' opacity-0'
-      }
-    }
-  }, [isChecked])
-  const baseStyle =
-    'absolute group-hover:opacity-100 z-10 right-2 top-1 rounded-2xl transition-all duration-500'
+
   return (
     <div
       draggable
       onDoubleClick={setImage}
-      className='transition-all border-[3px] border-transparent group hover:border-cyan-300 relative rounded-lg bg-transparent shadow-2xl  max-w-fit mb-4 overflow-hidden'
+      className='cursor-pointer transition-all border-[3px] border-transparent group hover:border-cyan-300 relative rounded-lg bg-transparent shadow-2xl  max-w-fit mb-4 overflow-hidden'
     >
-      <input
-        type='checkbox'
-        ref={inputRef}
-        className='absolute opacity-0 group-hover:opacity-100 z-10 right-2 top-1 rounded-2xl transition-all duration-500'
-        onChange={() => {
-          setChecked((previousValue) => !previousValue)
-        }}
-      />
+      <div className='relative'>
+        <label htmlFor={id} className=' absolute z-10 w-full h-10 opacity-0'>
+          
+        </label>
+        <input
+          id={id}
+          type='checkbox'
+          ref={inputRef}
+          className='absolute opacity-0 top-2 right-2 w-4 h-4 rounded-sm outline-none  group-hover:opacity-100 checked:opacity-100 checked:border-0 z-20 '
+        />
+      </div>
       <img
-        className='rounded-lg min-w-full transform-gpu group-hover:scale-110 group-hover:object-center active:scale-90 : transition-all'
+        className='rounded-lg min-w-full transform-gpu group-hover:scale-110 group-hover:object-center : transition-all'
         src={imageNameFilePath}
         alt={imageName}
         onError={({ currentTarget }) => {
