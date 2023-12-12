257939d951d17933aa475f12133dc5341c5fccc6
Diego Parra <diegomiguelp1996@gmail.com>
2023-12-12 16:47:11 +0100

    Add install script
    - Update npm scripts both in the daemon and electron app.
    - Update cli run app and daemon commands.
    - Update electron-builder.json.

--- cli/src/daemon_command.sh
+++ cli/src/daemon_command.sh
@@ -1,5 +1,5 @@
 run_daemon() {
-      waypaper-engine.AppImage --daemon 2>/dev/null || echo "waypaper engine is not on the path, make sure to add it before running this command"
+      waypaper-engine --daemon 2>/dev/null || echo "waypaper engine is not on the path, make sure to add it before running this command"
 }
 
 run_daemon & > /dev/null
--- cli/src/run_app_command.sh
+++ cli/src/run_app_command.sh
@@ -1,5 +1,5 @@
 run_app(){
-  waypaper-engine.AppImage --ozone-platform=wayland --enable-features=UseOzonePlatform,WaylandWindowDecorations 2>/dev/null || echo "waypaper engine is not on the path, make sure to add it before running this command"
+  waypaper-engine --ozone-platform=wayland --enable-features=UseOzonePlatform 2>/dev/null || echo "waypaper engine is not on the path, make sure to add it before running this command"
 }
 
 run_app >/dev/null &
