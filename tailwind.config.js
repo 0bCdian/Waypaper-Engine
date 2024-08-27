233eef5d6036ac23f7dcad4dfb6df258cd3a87ac
Diego Parra <101421807+0bCdian@users.noreply.github.com>
2024-08-27 20:57:14 +0200

    Delete old configs (#32)
    * Delete old configs
    
    * Modify tailwind.config.js to disable logger
    
    * Restore package.json?

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         utils: true, // adds responsive and modifier utility classes
         rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
         prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-        logs: true, // S
+        logs: false, // S
         themes: ["business"]
     }
 };
